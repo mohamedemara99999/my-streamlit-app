c037f82e16075a9ebcf5534fd6b6d33f850e0fa0
mohamedemara99999 <mohamedemara6199@gmail.com>
2025-11-21 00:48:00 +0200

    app.py

--- app.py
+++ app.py
@@ -111,59 +111,67 @@ else:
         df_final.drop(columns=['Originating_Number'], inplace=True)
         df_final['Count'] = df_final['Count'].astype(int)
         df_final = df_final.sort_values(by='Count', ascending=False)
-
+        # ====== استثناء الصف الأول بعد الهيدر =====
         if len(df_final) >= 2:
+            # أخذ الصف الثاني
             second_row_index = df_final.index[1]
-            second_b_number = df_final.at[second_row_index, 'B Number']
+            second_row = df_final.loc[[second_row_index]].copy()
+            # تعديل بياناته حسب الاستثناء
+            second_b_number = second_row.at[second_row_index, 'B Number']
             df_match = df[df['Originating_Number'].astype(str) == second_b_number]
             if not df_match.empty:
-                df_final.at[second_row_index, 'B Full Name'] = (
-                    str(df_match.iloc[0]['A_Number_Details_First_Name']) + " " + str(df_match.iloc[0]['A_Number_Details_Last_Name'])
+                second_row.at[second_row_index, 'B Full Name'] = (
+                str(df_match.iloc[0]['A_Number_Details_First_Name']) + " " +
+                str(df_match.iloc[0]['A_Number_Details_Last_Name'])
                 )
-                df_final.at[second_row_index, 'B Address'] = str(df_match.iloc[0]['ID_Num'])
-                df_final.at[second_row_index, 'B_NUMBER_SITE_ADDRESS'] = str(df_match.iloc[0]['MU_Site_Address'])
-                df_final.at[second_row_index, 'Latitude'] = ''
-                df_final.at[second_row_index, 'Longitude'] = ''
-                df_final.at[second_row_index, 'Map'] = ''
-                df_final.at[second_row_index, 'SMS'] = 0
-
-        # ====== imei sheet ======
-        imei_df = df.copy()
-        imei_df['IMEI_Number'] = imei_df['IMEI_Number'].astype(str)
-        imei_summary = imei_df.groupby('IMEI_Number').agg(
-            Count=('IMEI_Number','count'),
-            First_Use_Date=('Call_Start_Date','min'),
-            Last_Use_Date=('Call_Start_Date','max'),
-            First_Use_Address=('Site_Address','first'),
-            Last_Use_Address=('Site_Address','last')
-        ).reset_index()
-        imei_summary.rename(columns={'IMEI_Number':'IMEI'}, inplace=True)
-        imei_summary['Device Info'] = imei_summary['IMEI'].apply(lambda x: f'https://www.imei.info/calc/?imei={x}')
-        imei_summary = imei_summary[['IMEI','Count','Device Info','First_Use_Date','Last_Use_Date','First_Use_Address','Last_Use_Address']]
-        imei_summary['Count'] = imei_summary['Count'].astype(int)
-        imei_summary = imei_summary.sort_values(by='Count', ascending=False)
-
-        # ====== site sheet ======
-        site_df = df[['Site_Address','Latitude','Longitude','Call_Start_Date']].copy()
-        site_group = site_df.groupby('Site_Address').agg(
-            Count=('Site_Address','count'),
-            Map=('Latitude', lambda x: f'https://www.google.com/maps/search/?api=1&query={x.iloc[0]},{site_df.loc[x.index[0],"Longitude"]}'),
-            First_Use_Date=('Call_Start_Date','min'),
-            Last_Use_Date=('Call_Start_Date','max')
-        ).reset_index()
-        site_group['Count'] = site_group['Count'].astype(int)
-        site_group = site_group.sort_values(by='Count', ascending=False)
-        site_group = site_group[['Site_Address','Count','Map','First_Use_Date','Last_Use_Date']]
-
-        output = BytesIO()
-        with pd.ExcelWriter(output, engine='openpyxl') as writer:
-            df_final.to_excel(writer, sheet_name="calls", index=False)
-            imei_summary.to_excel(writer, sheet_name="imei", index=False)
-            site_group.to_excel(writer, sheet_name="site", index=False)
-        output.seek(0)
-
-        final_output = format_excel_sheets(output, header_color="228B22")
-        return final_output
+            second_row.at[second_row_index, 'B Address'] = str(df_match.iloc[0]['ID_Num'])
+            second_row.at[second_row_index, 'B_NUMBER_SITE_ADDRESS'] = str(df_match.iloc[0]['MU_Site_Address'])
+            second_row.at[second_row_index, 'Latitude'] = ''
+            second_row.at[second_row_index, 'Longitude'] = ''
+            second_row.at[second_row_index, 'Map'] = ''
+            second_row.at[second_row_index, 'SMS'] = 0
+            # حذف الصف من مكانه الأصلي
+            df_final = df_final.drop(second_row_index)
+            # إدراجه كأول صف بعد الهيدر
+            df_final = pd.concat([second_row, df_final], ignore_index=True)
+            # حفظ رقم الصف الأصفر للاستخدام في التلوين
+            df_final.attrs['highlight_yellow'] = 2  # أول صف بعد الهيدر في Excel يكون row=2
+            # ====== imei sheet ======
+            imei_df = df.copy()
+            imei_df['IMEI_Number'] = imei_df['IMEI_Number'].astype(str)
+            imei_summary = imei_df.groupby('IMEI_Number').agg(
+                Count=('IMEI_Number','count'),
+                First_Use_Date=('Call_Start_Date','min'),
+                Last_Use_Date=('Call_Start_Date','max'),
+                First_Use_Address=('Site_Address','first'),
+                Last_Use_Address=('Site_Address','last')
+                ).reset_index()
+            imei_summary.rename(columns={'IMEI_Number':'IMEI'}, inplace=True)
+            imei_summary['Device Info'] = imei_summary['IMEI'].apply(lambda x: f'https://www.imei.info/calc/?imei={x}')
+            imei_summary = imei_summary[['IMEI','Count','Device Info','First_Use_Date','Last_Use_Date','First_Use_Address','Last_Use_Address']]
+            imei_summary['Count'] = imei_summary['Count'].astype(int)
+            imei_summary = imei_summary.sort_values(by='Count', ascending=False)
+            # ====== site sheet ======
+            site_df = df[['Site_Address','Latitude','Longitude','Call_Start_Date']].copy()
+            site_group = site_df.groupby('Site_Address').agg(
+                Count=('Site_Address','count'),
+                Map=('Latitude', lambda x: f'https://www.google.com/maps/search/?api=1&query={x.iloc[0]},{site_df.loc[x.index[0],"Longitude"]}'),
+                First_Use_Date=('Call_Start_Date','min'),
+                Last_Use_Date=('Call_Start_Date','max')
+                ).reset_index()
+            site_group['Count'] = site_group['Count'].astype(int)
+            site_group = site_group.sort_values(by='Count', ascending=False)
+            site_group = site_group[['Site_Address','Count','Map','First_Use_Date','Last_Use_Date']]
+            # حفظ Excel
+            output = BytesIO()
+            with pd.ExcelWriter(output, engine='openpyxl') as writer:
+                df_final.to_excel(writer, sheet_name="calls", index=False)
+                imei_summary.to_excel(writer, sheet_name="imei", index=False)
+                site_group.to_excel(writer, sheet_name="site", index=False)
+                output.seek(0)
+                # تطبيق التنسيقات والهايبرلينك
+                final_output = format_excel_sheets(output)
+                return final_output
 
 # ================== تقرير فودافون ==================
 def generate_vodafone_report(df):
